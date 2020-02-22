d24bd9ad3ddb79e30c25c44e8537acb9cabeea1e
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-22 12:56:02 -0800

    Refactor _make_url()
    Make static since independent of instance.
    
    Remove conditioning for location grammer since Indeed.com interprets
    correctly even with missing commas.
    
    Fix bug with parsing entire location instead of parsing city only.

--- scraping/IndeedScraper.py
+++ scraping/IndeedScraper.py
@@ -7,19 +7,14 @@ class IndeedScraper:
     def __init__(self):
         self.results = []  # List of found Job entries
 
+    @staticmethod
     def _make_url(keywords="", city="", state="", zip_code=""):
         base_url = 'https://www.indeed.com/jobs?'
         query = urllib.parse.quote(keywords)
 
-        location_unparsed = city  # assume user will always enter a i
+        location_unparsed = " ".join([city, state, zip_code])
 
-        if state != "":
-            location_unparsed += f", {state}"
-        if zip_code != "":
-            location_unparsed += f" {zip_code}"
+        location = urllib.parse.quote(location_unparsed)
 
-            location = urllib.parse.quote(location_unparsed)
-
-        location = urllib.parse.quote(city)
         url = f"{base_url}q={query}&l={location}"  # &radius={radius}"
         return url
