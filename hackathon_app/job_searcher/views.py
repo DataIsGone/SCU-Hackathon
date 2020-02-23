921a98554377024d668b4af8d0202744b1821856
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-23 01:45:40 -0800

    Use template for-loop and dumb data
    Make home() post dummy job search results from a list of dicts. Pass
    list of dicts as context to render.
    
    Have home.html template use context in a for loop to display each dict
    as a separate job listing.
    
    Also use template if-statement for displaying the title of the
    page. Don't actually pass any title data in home()'s context.

--- hackathon_app/job_searcher/views.py
+++ hackathon_app/job_searcher/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+results = [
+    {'listing_id': 'p123',
+     'title': 'python dev',
+     'url': 'https://www.indeed.com/somelisting',
+     'company': 'Local big name company',
+     'location': 'Right down the road',
+     'summary': 'A fun python job!'
+    },
+    {'listing_id': 'p456',
+     'title': 'Data Scientist',
+     'url': 'https://www.indeed.com/this_other_listing',
+     'company': 'Far away startup',
+     'location': 'Across the country',
+     'summary': 'A fun new job where you can do ML!'
+    },
+]
+
+
 def home(request):
-    return HttpResponse(render(request, 'job_searcher/home.html'))
+    context = {'results': results}
+    return render(request, 'job_searcher/home.html', context)
 
 
 def about(request):
