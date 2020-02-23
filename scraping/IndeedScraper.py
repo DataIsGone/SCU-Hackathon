ecdfcb5d24f7aa81643125da450a591eb874c634
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-22 19:34:06 -0800

    Add method to save results as json file
    Add functionality to run from command line. Running from command line
    will save the result as a JSON file.

--- scraping/IndeedScraper.py
+++ scraping/IndeedScraper.py
@@ -1,12 +1,19 @@
+# IndeedScraper.py
+# script usage:
+# python IndeedScraper.py <filename>.json "Job Description" "City (optional)" "State (optional)" "zip code (optional)"
+
+
 import urllib
 import requests
+import json
+from sys import argv
 
 
 class IndeedScraper:
     _BASE_URL = "https://indreed.herokuapp.com/api/jobs?"
 
     def __init__(self):
-        self.results = []  # List of found Job entries
+        self._results = []
 
     @staticmethod
     def _make_url(keywords="", city="", state="", zip_code=""):
@@ -15,16 +22,28 @@ class IndeedScraper:
         location_unparsed = " ".join([city, state, zip_code])
 
         location = urllib.parse.quote(location_unparsed)
-
         url = f"{IndeedScraper._BASE_URL}q={query}&l={location}"
         return url
 
     def search(self, keywords="", city="", state="", zip_code=""):
-        response = requests.get(self._make_url(keywords, city,
-                                               state, zip_code))
-        return response.json()
+        url = IndeedScraper._make_url(keywords, city, state, zip_code)
+        response = requests.get(url)
+        self._results = response.json()
+
+    def get_results(self):
+        return self._results
+
+    def get_result_count(self):
+        return len(self._results)
+
+    def save_results_as_json(self, filename):
+        with open(filename, 'w') as outfile:
+            json.dump(self._results, outfile)
+
 
 
 if __name__ == '__main__':
     indeed = IndeedScraper()
-    results = indeed.search("chemistry", "Oakland", "CA")
+    indeed.search(*argv[2:])
+    print(indeed.get_results())
+    indeed.save_results_as_json(argv[1])
