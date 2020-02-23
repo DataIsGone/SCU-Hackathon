66cfe64cbc6aca985cf15769397659ec68fbb0ed
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-23 05:38:41 -0800

    Make two models Search and Result

--- hackathon_app/job_searcher/models.py
+++ hackathon_app/job_searcher/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Search(models.Model):
+    description = models.TextField()
+    city = models.TextField()
+    state = models.CharField(max_length=2)
+
+class Result(models.Model):
+    title = models.TextField()
+    summary = models.TextField()
+    location = models.TextField()
+    company = models.TextField()
+    url = models.URLField()
+    listing_id = models.TextField()
