4cc8b57d982a355bb6a89e0cef4dac7fb7594998
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-22 16:12:17 -0800

    Add Indeed search using indreed REST-API
    Change the base URL to use Indreed instead of Indeed.
    
    Make base URL class level constant.

--- scraping/IndeedScraper.py
+++ scraping/IndeedScraper.py
@@ -1,20 +1,30 @@
 import urllib
+import requests
 
 
 class IndeedScraper:
-    _BASE_URL = 'https://www.indeed.com/jobs?'
+    _BASE_URL = "https://indreed.herokuapp.com/api/jobs?"
 
     def __init__(self):
         self.results = []  # List of found Job entries
 
     @staticmethod
     def _make_url(keywords="", city="", state="", zip_code=""):
-        base_url = 'https://www.indeed.com/jobs?'
         query = urllib.parse.quote(keywords)
 
         location_unparsed = " ".join([city, state, zip_code])
 
         location = urllib.parse.quote(location_unparsed)
 
-        url = f"{base_url}q={query}&l={location}"  # &radius={radius}"
+        url = f"{IndeedScraper._BASE_URL}q={query}&l={location}"
         return url
+
+    def search(self, keywords="", city="", state="", zip_code=""):
+        response = requests.get(self._make_url(keywords, city,
+                                               state, zip_code))
+        return response.json()
+
+
+if __name__ == '__main__':
+    indeed = IndeedScraper()
+    results = indeed.search("chemistry", "Oakland", "CA")
