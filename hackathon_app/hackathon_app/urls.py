32780c5e49718516e3637e3ab6d3931e9517f07b
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-23 01:07:01 -0800

    Make base route to navigate to job search app
    Instead of 'job_searcher/' use ''.

--- hackathon_app/hackathon_app/urls.py
+++ hackathon_app/hackathon_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('job_searcher/', include('job_searcher.urls')),
+    path('', include('job_searcher.urls')),
 ]
