3576c87e28f8c5143204b03c2c5be979fb00a14b
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-23 01:14:11 -0800

    Create template for job_search.views.home()
    Render template instead of directly returning HTML string.
    
    Create template home.html in proper directory.

--- hackathon_app/job_searcher/views.py
+++ hackathon_app/job_searcher/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Job Searcher</h1>')
+    return HttpResponse(render(request, 'job_searcher/home.html'))
 
 
 def about(request):
