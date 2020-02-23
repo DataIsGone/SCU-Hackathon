99ee4578f0786baafb523a11dc688e64e1d8a2af
Jeremy Tsang <jeremy@jeremytsang.com>
2020-02-23 07:09:42 -0800

    Implement functional job search
    Create url, view, template for search result.
    
    Modify views.search(). Add a form that saves data to database. Pass
    along the saved object during a redirect to result page (along with its
    primary key (pk) to pass the correct object).
    
    Make template search.html such that it shows all results passed to it
    like in home.html.

--- hackathon_app/job_searcher/views.py
+++ hackathon_app/job_searcher/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .forms import SearchForm
+from django.shortcuts import redirect
 from job_searcher.modules.IndeedScraper import IndeedScraper
+from .models import Search
+
 
 indeed = IndeedScraper()
 indeed.search("Python Developer", "Santa Clara", "CA")
@@ -10,8 +14,23 @@ def home(request):
     context = {'results': indeed.get_results()}
     return render(request, 'job_searcher/home.html', context)
 
+def result(request, pk):
+    search = get_object_or_404(Search, pk=pk)
+    results = indeed.search(search.description, search.city, search.state)
+    return render(request, 'job_searcher/result.html', {'results': indeed.get_results()})
+    # return render(request, 'job_searcher/result.html', {'results': results})
+
+
 def search(request):
-    return HttpResponse(render(request, 'job_searcher/search.html', {'title': 'Search'}))
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.save()
+            return redirect('job_searcher_result', pk=search.pk)
+    else:
+        form = SearchForm()
+    return HttpResponse(render(request, 'job_searcher/search.html', {'title': 'Search', 'form': form}))
+
 
 def about(request):
     return HttpResponse(render(request, 'job_searcher/about.html', {'title': 'About'}))
